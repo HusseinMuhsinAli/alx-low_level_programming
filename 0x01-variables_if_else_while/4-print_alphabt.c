d215338350e7a889061f678cb150a15c36d85170
“HusseinMuhsinAli” <dirhussein@gmail.com>
2023-03-02 00:09:07 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/* 
- * This is the main function, which is the entry point of the program.
- * It returns an integer value to indicate the success or failure of the program.
+/*
+ * main - Entry point
+ * Return: 0 (Success)
  */
 int main(void)
 {
 char c;
-for (c = 'a'; c <= 'z'; c++) {
-if (c != 'q' && c != 'e') {
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'q' && c != 'e')
 putchar(c);
 }
-}
 putchar('\n');
-return 0;
+return (0);
 }
